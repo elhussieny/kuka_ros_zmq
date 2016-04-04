0194f22cd665288a2aa3cf4b537855aef6f8a250
Haitham El-Hussieny <haitham.elhussieny@gmail.com>
2016-04-04 22:31:32 +0900

    delete unused commands

--- src/kuka_interface.cpp
+++ src/kuka_interface.cpp
@@ -1,3 +1,4 @@
+/*هيثم الحسيني */
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <Eigen/Dense>
@@ -14,14 +15,14 @@ public:
 	ros::Subscriber kuka_sub;								//subscriber for internal ROS message (Desired Pose)
 	ros::Subscriber kuka_pub;                               //publisher for internal ROS message (Current Joints)
 	zmq::socket_t* publisher;								//zmq publisher for kuka
-	//zmq::socket_t* subscriber;								//zmq subscriber for kuka
-
+	//zmq::socket_t* subscriber;								//zmq subscriber from kuka
 
 	KUKAInterface(ros::NodeHandle nh_,zmq::context_t & context){
 		std::string input_pose_topic = "/kuka_interface/kuka_pose";
+
 		this->kuka_sub = nh_.subscribe(input_pose_topic,1,&KUKAInterface::KukaGoalCallback, this);
 		this->publisher=new zmq::socket_t(context,ZMQ_PUB);
-		publisher->bind("tcp://*:5555");//tcp://127.0.0.1:9998
+		publisher->bind("tcp://*:5555");
 
 
 	}
@@ -30,14 +31,13 @@ public:
 	void KukaGoalCallback(const geometry_msgs::PosePtr& intendedPose)
 	{
 
-		printf("Hello \n");
-		flatbuffers::FlatBufferBuilder fbb;
+											flatbuffers::FlatBufferBuilder fbb;
 		     					        	ros_kuka::flatbuffer::kukaDesPoseBuilder builder(fbb);
 
 		     					        	Vector3 pos(intendedPose->position.x,intendedPose->position.y,intendedPose->position.z);
 		     					        	Eigen::Quaterniond Rq = Eigen::Quaterniond(intendedPose->orientation.x,intendedPose->orientation.y,intendedPose->orientation.z,intendedPose->orientation.w);
-		     					          	Eigen::Vector3d eulerAngles=Rq.toRotationMatrix().eulerAngles(2,1,0); // ZXZ
-		     					           	RPY rot(eulerAngles[0],eulerAngles[1],eulerAngles[2]); // spped testing
+		     					          	Eigen::Vector3d eulerAngles=Rq.toRotationMatrix().eulerAngles(2,1,0); // ZYX convention
+		     					           	RPY rot(eulerAngles[0],eulerAngles[1],eulerAngles[2]); //
 		     					        	builder.add_position(&pos);
 		     			 		        	builder.add_rotation(&rot);
 		     					        	auto response=builder.Finish();
@@ -55,15 +55,12 @@ public:
 	}
 
 };
-/*------------------------------------------------------------------------------------*/
-
 /*-----------------------------------------------------------------------------------------------------*/
 int main(int argc, char** argv)
  {
    ros::init(argc, argv, "kuka_control");
    ros::NodeHandle nh_;
    zmq::context_t context (1);
-  // zmq::socket_t publisher (context, ZMQ_PUB);
    KUKAInterface kukaObject(nh_,context);
    ros::spin();
   return 0;
